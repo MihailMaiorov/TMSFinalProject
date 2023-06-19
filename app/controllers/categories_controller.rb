62783e1d903586b94608b071300005f90a5eb006
MihailMaiorov <7351943@gmail.com>
2023-06-19 23:44:26 +0300

    add catigories_controller, view and routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,18 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[edit show]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @product = @category.products.build
+    @products = @category.products.order created_at: :desc
+  end
+end
+
+private
+
+def set_category
+  @category = Category.find params[:id]
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ApplicationRecord
-  PRICE_REGEXP = /\A\d+(?:\.\d{0,2})?\z/
-
   belongs_to :user
   belongs_to :category
 
   validates :name, presence: true, length: { minimum: 2 }
 
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
+
+  def formatted_create_at
+    created_at.strftime('%Y-%m-%d %H-%M-%S')
+  end
 end
