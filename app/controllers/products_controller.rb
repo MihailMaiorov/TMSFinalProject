fa56322988af1999a37e650ef5d7ad9ae9a93ae2
MihailMaiorov <7351943@gmail.com>
2023-06-21 01:40:57 +0300

    add pictures active_storage to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,7 +54,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, pictures: [])
   end
 
   def product_create_params
