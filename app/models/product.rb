767e5bd89fef8367a3513c91f0c46c8392a7cd0c
MihailMaiorov <7351943@gmail.com>
2023-06-21 00:59:59 +0300

    add pictures relation and validation at model/product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many_attached :pictures
 
   validates :name, presence: true, length: { minimum: 2 }
 
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
+
+  validates :pictures, blob: { content_type: %w[image/png image/jpg image/jpeg], size_range: 1..5.megabytes }
 
   def formatted_create_at
     created_at.strftime('%Y-%m-%d %H-%M-%S')
