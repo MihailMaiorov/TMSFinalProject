53a230b304ecaca00e59b623a132d2917b74364e
MihailMaiorov <7351943@gmail.com>
2023-06-20 23:08:55 +0300

    add edit and destroy links in products views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,10 +19,8 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product.save
         format.html { redirect_to category_path(@category), notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -30,11 +28,9 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
+        format.html { redirect_to category_path(@category), notice: 'Product was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,8 +39,7 @@ class ProductsController < ApplicationController
     @product.destroy
 
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to category_path(@category), notice: 'Product was successfully destroyed.' }
     end
   end
 
