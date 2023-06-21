dcc92d193b48a5da6c1e7ad6bc8061a3d934b51a
MihailMaiorov <7351943@gmail.com>
2023-06-22 01:45:32 +0300

    remove products routes and rework views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :products
+
   resources :categories, only: %i[index show] do
     resources :products
   end
