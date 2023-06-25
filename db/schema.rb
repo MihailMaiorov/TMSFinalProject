4f25dd3fce0b3c24afc334875867200dbe0d6775
MihailMaiorov <7351943@gmail.com>
2023-06-26 02:42:32 +0300

    create review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_202806) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_25_234142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_202806) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
