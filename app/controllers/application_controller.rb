1839142ebb735c2ec23d44204969612fe8e66ea1
MihailMaiorov <7351943@gmail.com>
2023-07-01 02:48:47 +0300

    create current cart and items method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Internationalization
+
+  def current_cart
+    @current_cart ||= Cart.find_or_create_by(user: current_user)
+  end
+
+  def current_items
+    current_cart.cart_items
+  end
+
+  helper_method :current_cart, :currernt_items
 end
