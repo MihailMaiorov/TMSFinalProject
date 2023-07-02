5a9a9d89e4b51df476ad8ffa9f1c8c62bd8de034
MihailMaiorov <7351943@gmail.com>
2023-07-02 23:22:23 +0300

    add role select in registration user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,14 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
 
+    def create
+      super do |user|
+        if user.persisted?
+          user.add_role(params[:role])
+        end
+      end
+    end
+
     protected
 
     def configure_sign_up_params
