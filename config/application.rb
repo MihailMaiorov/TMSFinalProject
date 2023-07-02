b0777816d0883b868337d172c6fc6ffe365d1d79
MihailMaiorov <7351943@gmail.com>
2023-07-03 01:02:50 +0300

    add redis and sidekiq to Gemfile

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Marketplace
     config.i18n.available_locales = %i[en ru]
     config.i18n.default_locale = :en
 
+    config.active_storage.variant_processor = :mini_magick
+    config.active_job.queue_adapter = :sidekiq
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
