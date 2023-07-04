50002c8f782bf02f98a9ed17e85e50fb99afd465
MihailMaiorov <7351943@gmail.com>
2023-07-04 04:51:56 +0300

    rework cart_item factories

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,9 @@
-import { Application } from "@hotwired/stimulus"
+import "@hotwired/turbo-rails"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
--- spec/factories/cart_item.rb
+++ spec/factories/cart_item.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :cart_item do
-    association :product, factory: :product
-    # assotiation :cart, factory: :cart
-
     sequence(:quantity)
   end
 end
