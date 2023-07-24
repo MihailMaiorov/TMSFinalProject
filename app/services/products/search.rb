f8160eab98969ab5efd410c8872145ff2bae4e57
MihailMaiorov <7351943@gmail.com>
2023-07-25 00:23:05 +0300

    rename variables in search method

--- app/services/products/search.rb
+++ app/services/products/search.rb
@@ -3,17 +3,17 @@
 module Products
   class Search
     class << self
-      def call(category_title:, q:)
+      def call(category_title:, query:)
         if category_title.blank?
-          @q = Product.in_stock.ransack(q)
+          result = Product.in_stock.ransack(query)
         else
-          @category_id = Category.find_by(title: category_title).id
-          @q = Product.in_stock.where(category_id: @category_id).ransack(q)
+          category_id = Category.find_by(title: category_title).id
+          result = Product.in_stock.where(category_id: category_id).ransack(query)
         end
 
-        @products = @q.result.includes(:category)
+        products = result.result.includes(:category)
 
-        [@q, @products]
+        [result, products]
       end
     end
   end
